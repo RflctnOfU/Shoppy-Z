9b3cf4b8837cca41a7dcf15a27511135f9cf335e
Kris Marshall <rflctnofu@hotmail.com>
2024-01-17 14:52:50 -0500

    initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import { unstable_noStore as noStore } from "next/cache";
+import Link from "next/link";
+
+// import { CreatePost } from "@/app/_components/create-post";
+import { getServerAuthSession } from "@/server/auth";
+import { api } from "@/trpc/server";
+
+export default async function Home() {
+  noStore();
+  const hello = await api.post.hello.query({ text: "from tRPC" });
+  const session = await getServerAuthSession();
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      Hello World
+    </main>
+  );
+}
